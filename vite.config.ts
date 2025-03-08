7883b6749cca52e9d4527553f3439a0f1cfa8a30
Andrew Laskey <andrew.laskey@gmail.com>
2025-03-08 15:03:23 -0800

    fix: itch paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ const deployTarget = process.env.DEPLOY_TARGET || 'github';
 const getBasePath = () => {
   switch (deployTarget) {
     case 'itch':
-      return '/'; // Root path for itch.io
+      return ''; // Root path for itch.io, no forward slash
     case 'github':
     default:
       return '/generation-ship-2/'; // Subfolder for GitHub Pages
