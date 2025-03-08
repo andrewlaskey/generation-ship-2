6ad22657131773c6414d1e33db8bb49495d4009b
Andrew Laskey <andrew.laskey@gmail.com>
2025-03-08 14:52:06 -0800

    fix: adjust base url for itch deploys

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite';
 
+const deployTarget = process.env.DEPLOY_TARGET || 'github';
+
+const getBasePath = () => {
+  switch (deployTarget) {
+    case 'itch':
+      return '/'; // Root path for itch.io
+    case 'github':
+    default:
+      return '/generation-ship-2/'; // Subfolder for GitHub Pages
+  }
+};
+
 export default defineConfig({
-    base: '/generation-ship-2/',
+  base: getBasePath()
 });
